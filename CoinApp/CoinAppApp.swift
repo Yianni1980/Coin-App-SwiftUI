e091a88de5bf85a3253b03a66c8b4cedc3a4c6b5
Yianni1980 <ioagian1980@gmail.com>
2023-01-13 14:12:11 +0200

    Initial Commit

---
+++ CoinApp/CoinAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinAppApp.swift
+//  CoinApp
+//
+//  Created by ioannis on 13/1/23.
+//
+
+import SwiftUI
+
+@main
+struct CoinAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
